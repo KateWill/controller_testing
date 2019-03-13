006732bc08f398d34bef27d14866cfdc3ccb25bd
Katelyn Williamson <katelynrwilliamson@gmail.com>
2019-03-12 21:04:22 -0600

    added controllers to test controller tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,3 @@
-## code that runs in all of our tests
-
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
@@ -8,10 +6,9 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 require 'simplecov'
-require 'devise'
-require 'support/controller_macros'
+require "devise"
+require "support/controller_macros"
 SimpleCov.start
-
 
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
@@ -19,8 +16,11 @@ Shoulda::Matchers.configure do |config|
     with.test_framework :rspec
 
     with.library :rails
-    
   end
+end
+
+RSpec.configure do |config|
+  config.include FactoryBot::Syntax::Methods
 end
 
 
@@ -51,6 +51,20 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
+
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.extend ControllerMacros, type: :controller
+  
+  config.before(:suite) do 
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
